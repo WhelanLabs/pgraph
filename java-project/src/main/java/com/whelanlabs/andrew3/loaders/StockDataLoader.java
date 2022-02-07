85502cd576f864dc225e2695a13f34f2db8ba3ad
John Whelan <WhelanLabs@users.noreply.github.com>
2022-02-06 18:33:28 -0600

    logging & clean-up
    add logging and cleanup

--- java-project/src/main/java/com/whelanlabs/andrew3/loaders/StockDataLoader.java
+++ java-project/src/main/java/com/whelanlabs/andrew3/loaders/StockDataLoader.java
@@ -8,11 +8,15 @@ import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.arangodb.ArangoCollection;
 import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDatabase;
 import com.arangodb.entity.BaseDocument;
 import com.arangodb.entity.BaseEdgeDocument;
+import com.arangodb.entity.CollectionEntity;
 import com.arangodb.entity.CollectionPropertiesEntity;
 import com.arangodb.entity.CollectionType;
 import com.arangodb.mapping.ArangoJack;
@@ -25,6 +29,8 @@ public class StockDataLoader {
    private static ArangoDatabase db;
 
    private static final LocalDate epoch = LocalDate.ofEpochDay(0);
+
+   private static Logger logger = LogManager.getLogger(StockDataLoader.class);
 
    public static void setUp() throws InterruptedException, ExecutionException {
       arangoDB = new ArangoDB.Builder().user("root").password("openSesame").serializer(new ArangoJack()).build();
@@ -47,29 +53,29 @@ public class StockDataLoader {
    public static void main(String[] args) throws InterruptedException, ExecutionException {
       setUp();
 
-      createCollection("node_types");
+      createNodeCollection("node_types");
       final ArangoCollection node_types = db.collection("node_types");
-      createCollection("edge_types");
+      createNodeCollection("edge_types");
       final ArangoCollection edge_types = db.collection("edge_types");
 
-      final BaseDocument v1 = new BaseDocument();
+      BaseDocument v1 = new BaseDocument();
       v1.setKey("dates");
-      node_types.insertDocument(v1);
+      v1 = upsertNode(node_types, v1);
 
-      final BaseDocument v2 = new BaseDocument();
+      BaseDocument v2 = new BaseDocument();
       v2.setKey("tickers");
-      node_types.insertDocument(v2);
+      v2 = upsertNode(node_types, v2);
 
-      final BaseDocument v3 = new BaseDocument();
+      BaseDocument v3 = new BaseDocument();
       v3.setKey("marketData");
       v3.addAttribute("left_type", "dates");
       v3.addAttribute("right_type", "tickers");
-      edge_types.insertDocument(v3);
+      v3 = upsertNode(edge_types, v3);
 
-      createCollection("dates");
+      createNodeCollection("dates");
       final ArangoCollection dates = db.collection("dates");
 
-      createCollection("tickers");
+      createNodeCollection("tickers");
       final ArangoCollection tickers = db.collection("tickers");
 
       createEdgeCollection("marketData");
@@ -79,11 +85,11 @@ public class StockDataLoader {
       try {
          String filename = "D:\\sandbox\\2022_andrew\\andrew\\andrew\\fetchers\\stock_data_fetcher\\data\\AA_2020-05-07.txt";
 
-         final BaseDocument ticker = new BaseDocument();
+         BaseDocument ticker = new BaseDocument();
          ticker.setKey("AA");
-         tickers.insertDocument(ticker);
+         ticker = upsertNode(tickers, ticker);
 
-         System.out.println("reading: " + filename);
+         logger.info("reading: " + filename);
          reader = new BufferedReader(new FileReader(filename));
          String line = reader.readLine();
          Boolean headerLine = true;
@@ -91,28 +97,28 @@ public class StockDataLoader {
             if (headerLine) {
                headerLine = false;
             } else {
-               System.out.println(line);
+               // System.out.println(line);
                String[] tokens = line.split(",");
                LocalDate date = LocalDate.parse(tokens[0]);
 
-               final BaseDocument stockDate = new BaseDocument();
+               BaseDocument stockDate = new BaseDocument();
                stockDate.setKey(date.toString());
                Long dayNumber = ChronoUnit.DAYS.between(epoch, date);
-               System.out.println("Days: " + dayNumber);
+               // System.out.println("Days: " + dayNumber);
                stockDate.addAttribute("date", dayNumber);
-               dates.insertDocument(stockDate);
+               stockDate = upsertNode(dates, stockDate);
 
-               // TODO: add marketData (Open,High,Low,Close,Adj Close,Volume)
                BaseEdgeDocument stockDay = new BaseEdgeDocument();
                stockDay.setFrom(stockDate.getId());
                stockDay.setTo(ticker.getId());
+               stockDay.setKey(stockDate.getKey() + ":" + ticker.getKey());
                stockDay.addAttribute("open", tokens[1]);
                stockDay.addAttribute("high", tokens[2]);
                stockDay.addAttribute("low", tokens[3]);
                stockDay.addAttribute("close", tokens[4]);
                stockDay.addAttribute("adjClose", tokens[5]);
                stockDay.addAttribute("volume", tokens[6]);
-               marketData.insertDocument(stockDay);
+               stockDay = upsertEdge(marketData, stockDay);
 
             }
 
@@ -123,11 +129,11 @@ public class StockDataLoader {
 
          final BaseDocument badDate = new BaseDocument();
          badDate.setKey(UUID.randomUUID().toString());
-         dates.insertDocument(badDate);
+         upsertNode(dates, badDate);
 
          // FOR doc IN collection COLLECT WITH COUNT INTO length RETURN length
          CollectionPropertiesEntity count = dates.count();
-         System.out.println("count: " + count.getCount());
+         logger.info("count: " + count.getCount());
 
       } catch (IOException e) {
          e.printStackTrace();
@@ -137,14 +143,67 @@ public class StockDataLoader {
 
    }
 
-   private static void createEdgeCollection(String collectionType) {
-
-      db.createCollection(collectionType, new CollectionCreateOptions().type(CollectionType.EDGES));
-
+   private static BaseDocument upsertNode(final ArangoCollection collection, final BaseDocument element) {
+      BaseDocument result = null;
+      try {
+         if (!collection.documentExists(element.getKey())) {
+            collection.insertDocument(element);
+            result = element;
+         } else {
+            logger.debug("Fetch already existing element. (key=" + element.getKey() + ")");
+            result = collection.getDocument(element.getKey(), BaseDocument.class);
+         }
+      } catch (Exception e) {
+         logger.error(element.toString());
+         throw e;
+      }
+      return result;
    }
 
-   private static void createCollection(String collectionType) {
-      db.createCollection(collectionType);
+   private static BaseEdgeDocument upsertEdge(final ArangoCollection collection, final BaseEdgeDocument element) {
+      BaseEdgeDocument result = null;
+      try {
+         if (!collection.documentExists(element.getKey())) {
+            collection.insertDocument(element);
+            result = element;
+         } else {
+            logger.debug("Fetch already existing element. (key=" + element.getKey() + ")");
+            result = collection.getDocument(element.getKey(), BaseEdgeDocument.class);
+         }
+      } catch (Exception e) {
+         logger.error(element.toString());
+         throw e;
+      }
+      return result;
+   }
+
+   private static CollectionEntity createEdgeCollection(String collectionName) {
+      CollectionEntity result = null;
+      ArangoCollection collection = db.collection(collectionName);
+      if (!collection.exists()) {
+         result = db.createCollection(collectionName, new CollectionCreateOptions().type(CollectionType.EDGES));
+      } else {
+         result = collection.getInfo();
+         logger.debug("createEdgeCollection - result.getType() = " + result.getType());
+         if (!"EDGES".equals(result.getType().toString())) {
+            throw new RuntimeException("Non-DOCUMENT collection already exsists. (" + result.getType() + ")");
+         }
+      }
+      return result;
+   }
+
+   private static CollectionEntity createNodeCollection(String collectionName) {
+      CollectionEntity result = null;
+      ArangoCollection collection = db.collection(collectionName);
+      if (!collection.exists()) {
+         result = db.createCollection(collectionName);
+      } else {
+         result = collection.getInfo();
+         if (!"DOCUMENT".equals(result.getType().toString())) {
+            throw new RuntimeException("Non-DOCUMENT collection already exsists. (" + result.getType() + ")");
+         }
+      }
+      return result;
    }
 
 }
