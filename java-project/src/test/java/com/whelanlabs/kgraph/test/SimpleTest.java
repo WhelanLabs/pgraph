e7853c0c4f4563148e920c60bbaae6e53bed02c5
John Whelan <WhelanLabs@users.noreply.github.com>
2022-04-02 13:28:21 -0500

    testing query subclass

---
+++ java-project/src/test/java/com/whelanlabs/kgraph/test/SimpleTest.java
@@ -0,0 +1,55 @@
+package com.whelanlabs.kgraph.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.arangodb.ArangoCollection;
+import com.arangodb.entity.BaseDocument;
+import com.whelanlabs.kgraph.engine.KnowledgeGraph;
+import com.whelanlabs.kgraph.engine.Node;
+import com.whelanlabs.kgraph.engine.QueryClause;
+
+public class SimpleTest {
+
+   private static KnowledgeGraph kGraph = null;
+   private static String tablespace_name = "SimpleTests_db";
+   private static Logger logger = LogManager.getLogger(SimpleTest.class);
+
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception {
+      // KnowledgeGraph.removeTablespace(tablespace_name);
+      kGraph = new KnowledgeGraph(tablespace_name);
+      kGraph.flush();
+   }
+
+   @AfterClass
+   public static void tearDownAfterClass() throws Exception {
+      kGraph.cleanup();
+   }
+
+   @Test
+   public void queryNodes_singleClause_getResult() throws Exception {
+      final ArangoCollection testCollection = kGraph.getNodeCollection("testCollection");
+      final BaseDocument testDoc = new BaseDocument(kGraph.generateKey());
+      testDoc.addAttribute("foo", "bar");
+      testDoc.addAttribute("xname", "Homer");
+      kGraph.upsertNode(testCollection, testDoc);
+      Node addedDoc = kGraph.getNodeByKey(testDoc.getKey(), testCollection.name());
+      assertNotNull(addedDoc);
+      logger.debug("addedDoc = " + addedDoc.toString());
+
+      QueryClause queryClause = new QueryClause("foo", QueryClause.Operator.EQUALS, "bar");
+      List<Node> results = kGraph.queryNodes(testCollection, queryClause);
+      assertNotNull(results);
+      logger.debug("results = " + results);
+      assert (results.size() > 0) : "results.size() = " + results.size();
+   }
+
+}
